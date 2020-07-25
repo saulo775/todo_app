cf4fe96c944c387477dc69c94cbfe338cf135cf6
saulo775 <59702227+saulo775@users.noreply.github.com>
2020-07-25 14:35:13 -0300

    commit 1 - Adcionando um item

---
+++ lib/main.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'models/item.dart';
+
+
+
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Todo App',
+
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+      ),
+      home: HomePage(),
+
+    );
+  }
+}
+
+
+class HomePage extends StatefulWidget {
+  var items = new List<Item>();
+
+  HomePage(){
+    items = [];
+    items.add(Item(title:"Item 1", done: false));
+    items.add(Item(title:"Item 2", done: true));
+    items.add(Item(title:"Item 3", done: false));
+
+  }
+
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var newTaskCtrl = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:  TextFormField(
+          controller: newTaskCtrl,
+          keyboardType: TextInputType.text,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 24,
+          ),
+          decoration: InputDecoration(
+            labelText: "Nova Tarefa",
+            labelStyle: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+
+        ),
+
+      ),
+      body: ListView.builder(
+        itemCount: widget.items.length,
+        itemBuilder:(BuildContext ctxt, int index){
+          final item = widget.items[index];
+          return CheckboxListTile(
+            title: Text(item.title),
+            key: Key(item.title),
+            value: item.done,
+            onChanged: (value) {
+              setState((){
+                item.done = value;
+              });
+            },
+          );
+        },
+      ),
+    );
+  }
+}
